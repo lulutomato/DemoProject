fb5e014dcefc3534b296552a7ac49271ab0c538b
chajungho <slomo.littlecha@gmail.com>
2024-03-11 10:34:14 +0900

    수정

--- src/main/webapp/js/bookcase.js
+++ src/main/webapp/js/bookcase.js
@@ -5,10 +5,11 @@ function tossPayInit() {
 	// 토스페이먼츠 회원가입하기 전이라면, 아래 문서용 테스트 키를 사용하세요. 문서용 테스트 키는 _docs_가 포함되어 있어요.
 	// 토스페이먼츠에 회원가입했다면, 개발자센터에서 내 테스트 상점 키를 확인하세요.
 	// 로그인한 상태라면, 문서에 있는 클라이언트 키, 시크릿 키가 내 테스트 키로 바뀌어요.
-	const clientKey = '{clientKey}'
+	const clientKey = 'test_ck_4yKeq5bgrpPeGDGY4MaJrGX0lzW6'
 
-    const customerKey = "VUEWF1yYWn17zOh3L6Wot" // 내 상점에서 고객을 구분하기 위해 발급한 고객의 고유 ID(임의값)
+    const customerKey = crypto.randomUUID() //"VUEWF1yYWn17zOh3L6Wot" // 내 상점에서 고객을 구분하기 위해 발급한 고객의 고유 ID(임의값)
 //    const coupon = document.getElementById("coupon-box")
+	console.log('customerkey',customerKey)
     const button = document.getElementById("payment-button")
     // ------  결제위젯 초기화 ------ 
     // 비회원 결제에는 customerKey 대신 ANONYMOUS를 사용하세요.
@@ -43,8 +44,8 @@ function tossPayInit() {
     const payObj = {
         orderId: obj.bcode +'_' + orderid,            
         orderName: obj.title,                 
-        successUrl: window.location.origin + "/DemoProj/pay/success",  
-        failUrl: window.location.origin + "/DemoProj/pay/fail",        
+        successUrl: window.location.origin + "/DemoProject/pay/success",  
+        failUrl: window.location.origin + "/DemoProject/pay/fail",        
         customerName: obj.saleuser
     }
     console.log(payObj)
